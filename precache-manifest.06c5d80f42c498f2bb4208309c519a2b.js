c250d8d2538df554da2013371b4843607a3414b4
Andrew Chow <chowlick@gmail.com>
2019-04-24 09:57:30 -0700

    Updates

---
+++ precache-manifest.06c5d80f42c498f2bb4208309c519a2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a56933586e21782d72fe",
+    "url": "/tic-tac-toe/static/css/main.6b28e78e.chunk.css"
+  },
+  {
+    "revision": "a56933586e21782d72fe",
+    "url": "/tic-tac-toe/static/js/main.2e512487.chunk.js"
+  },
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/tic-tac-toe/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "2e6551d39e8a88d6956ab87c214102bb",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
